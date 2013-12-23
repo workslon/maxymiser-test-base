e9e5e03a1a70c3741d94cfcfcac359bef5e0a9bd
Anton Merkulov <antonio.merkulov.com>
2013-12-23 14:28:18 +0200

    template test added

--- tests/spec/template-test.js
+++ tests/spec/template-test.js
@@ -17,14 +17,18 @@
       });
 
       async.it("setHTML", function(done) {
-        //TODO: call testBase methods
-        expect(true).toBe(true);
+        var mm_inner_HTML = 'some text with {{var1}} and {{var2}} and {{var1}} again :)';
+        mm_inner_HTML = testBase.template.setHTML('htmlSnippet1', mm_inner_HTML);
+        expect(mm_inner_HTML).toBe('');
         done();
       });
 
       async.it("getHTML", function(done) {
-        //TODO: call testBase methods
-        expect(true).toBe(true);
+        var resultText = testBase.template.getHTML('htmlSnippet1', {
+          "var1": '(this is a replaced variable 1)',
+          "var2": '(this is a replaced variable 2)'
+        });
+        expect(resultText).toBe('some text with (this is a replaced variable 1) and (this is a replaced variable 2) and (this is a replaced variable 1) again :)');
         done();
       });
 
